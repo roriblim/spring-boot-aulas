7d5a5ffbd85768c40f5b1370a377048fd8935850
rosana <rosanariblim@gmail.com>
2021-11-18 17:20:05 -0300

    parte final migrations

---
+++ scaffold-person-migrations/src/main/resources/db/migration/V3__teste_Table_Person3.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(3, 'neverland', 'michael jackson', 'male', ''),
+(4, 'florianopolis', 'gustavo', 'male', 'kuerten');
