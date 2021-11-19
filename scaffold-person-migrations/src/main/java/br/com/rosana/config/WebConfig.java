0a63762be18adf70b964a9f693f017f0d6288693
rosana <rosanariblim@gmail.com>
2021-11-19 07:31:22 -0300

    content negotiation via header

--- scaffold-person-migrations/src/main/java/br/com/rosana/config/WebConfig.java
+++ scaffold-person-migrations/src/main/java/br/com/rosana/config/WebConfig.java
@@ -10,20 +10,41 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 
-	//exemplo para consulta: 
-	//localhost:8080/api/person/v1/6?mediaType=xml
-	//localhost:8080/api/person/v1/6?mediaType=json
-	//localhost:8080/api/person/v1?mediaType=json
-	//localhost:8080/api/person/v1?mediaType=xml
+
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(true)
-			.parameterName("mediaType")
-			.ignoreAcceptHeader(true)
-			.useRegisteredExtensionsOnly(false)
-			.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);
+		
+		//PARA USAR VIA QUERY PARAMETER:
+		//exemplo para consulta: 
+		//localhost:8080/api/person/v1/6?mediaType=xml
+		//localhost:8080/api/person/v1/6?mediaType=json
+		//localhost:8080/api/person/v1?mediaType=json
+		//localhost:8080/api/person/v1?mediaType=xml
+		
+		//COM QUERY PARAM:
+		 configurer.favorParameter(true) 
+		 .parameterName("mediaType")
+		 .ignoreAcceptHeader(true) 
+		 .useRegisteredExtensionsOnly(false)
+		 .defaultContentType(MediaType.APPLICATION_JSON) 
+		 .mediaType("json", MediaType.APPLICATION_JSON) 
+		 .mediaType("xml", MediaType.APPLICATION_XML);
+		  
+		
+		
+		//PARA USAR VIA HEADER PARAMETER:
+		//simplesmente coloque no header, no postman, 
+		//na hora de enviar a requisição, a key Accept com o value application/json ou application/xml
+		 
+		/*//COM HEADER PARAM:
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON)
+		 * .mediaType("json",MediaType.APPLICATION_JSON) .mediaType("xml",
+		 * MediaType.APPLICATION_XML);
+		 */
+		
+		
 	}
 }
